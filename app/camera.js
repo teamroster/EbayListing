7f03ba410a529983a5671fb41ce87aaf677ea62b
teamroster <github@teamroster.com>
2019-11-04 15:27:01 -0500

    Add files via upload

--- app/camera.js
+++ app/camera.js
@@ -9,10 +9,13 @@ const cameraTrigger = document.querySelector("#camera-trigger");
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
-
+    // if('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices){
+    //     console.log("Let's get this party started")
+    // }
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
+            //track = stream.getTracks()[0];
             cameraView.srcObject = stream;
         })
         .catch(function(error) {
@@ -22,8 +25,8 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    //cameraSensor.width = cameraView.videoWidth;
-    //cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
